ef22b4391be778a71688c53d4c7d137c4a300825
Paul G H <paulghin@hotmail.com>
2015-08-21 15:33:19 -0500

    first commit of modified code

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,27 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+##    Based on the makeVector and cachemean functions in example for this assignment.
+##    Essentially, replace vector with matrix and solve for inverse instead of finding mean.
+##    Just a disclaimer: MakeVector and cachemean were modified to create these two functions.
 
-## Write a short comment describing this function
+
+## Write a short comment describing this function.
+##  makeCacheMatrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setmatinv <- function(invMat) m <<- invMat
+  getmatinv <- function() m
+  list(set = set, get = get,
+       setmatinv = setmatinv,
+       getmatinv = getmatinv)
+  
 }
 
 
@@ -12,4 +29,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatinv()
+  
+  if(!is.null(m)) {     #inverse is cached
+    message("getting cached data")
+    return(m)
+  }
+  
+  message("inverse not cached, calculating inverse")  
+  data <- x$get()       #not cached, calculate inverse
+  m <- solve(data)
+  x$setmatinv(m)
+  m
 }
